577003f3ba991dd97499b41e71a20cea651fb2a7
kimuit117 <kemoikiuita@gmail.com>
2022-10-10 15:19:04 +0300

    adds a table header

---
+++ src/components/Tableheader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Datasort.css"
+
+function Tableheader() {
+  return (
+         <div className='tableheader'>
+ <table>  <tr className='topRankingGrid'>
+    <th>Member Name</th>
+    <th>Contact</th>
+    <th>E-mail Adress</th>
+    <th>house Address</th>
+  </tr></table>
+        </div>
+  )
+}
+
+export default Tableheader;
