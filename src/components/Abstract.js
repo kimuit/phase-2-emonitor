a641e228ece90a16bd2c57d55447e9e7060b6c79
kimuit117 <kemoikiuita@gmail.com>
2022-10-10 16:12:47 +0300

    update the js abstract file

---
+++ src/components/Abstract.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./Datasort.css"
+import Search from './Search'
+
+function Abstract() {
+  return <>
+    <div className="outer-container">
+        <br/>
+<h1 className='abstract_header'>Promotiming community security services by facilitating security services</h1>
+<p className='abstract_header'>Creating quality urban lifestyles, biulding stronger communities.</p>
+<br/>
+<Search/>
+<h1 className='abstract_header'>Why we exist</h1>
+ <p className='abstract_header'>
+    We are built to make your work easier. Like any online ecommerce. 
+    AgriPlus provide a great platform for farmers to reach their customers throughout the country.
+    We have made it easier to shop from anywhere through the platform.
+</p>
+<br/>
+<h3>list of estate  members</h3>
+    </div>
+  </>
+}
+
+export default Abstract
